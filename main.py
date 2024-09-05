d71e97a96de58dd686d14e3cb8cba1e151e5f66a
windsnow1025 <windsnow1025@gmail.com>
2024-09-05 02:20:48 -0700

    chore

--- main.py
+++ main.py
@@ -4,27 +4,21 @@ from src.scholaro_gpa import ScholaroGPA
 from src.wes_igpa import WESiGPA
 from excel_reader import read_excel_data
 
-# Load the Excel file
 excel_file = 'data/sign_score.xlsx'  # Replace with your Excel file path
 
 try:
-    # Read data from the Excel file
     course_data = read_excel_data(excel_file)
 
-    # Initialize the WebDriver (e.g., Chrome)
-    driver = webdriver.Chrome()  # Make sure chromedriver is in your PATH
+    driver = webdriver.Chrome()
 
-    # Create an instance of WESiGPAScraper
+    # Create an instance of WESiGPA / ScholaroGPA Scraper
     # scraper = WESiGPA(driver)
     scraper = ScholaroGPA(driver)
 
-    # Add courses from the Excel data
     scraper.add_courses_from_data(course_data)
 
-    # Keep the browser open for you to see the result
     input("Press Enter to close the browser...")
 
-    # Close the browser after you press Enter
     driver.quit()
 
 except Exception as e:
